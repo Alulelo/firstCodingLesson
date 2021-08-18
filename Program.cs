face03aa803432e7d1d25a337a9d846835de70ef
Alulelo <hotpotatou@gmail.com>
2021-08-18 22:19:25 +0500

    bhai ki frist comit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+            Console.WriteLine("There was a man named " + characterName);
+            Console.WriteLine("He was "+ characterAge +" years old ");
+            characterName = "Mike";
+            characterAge = 25;
+            Console.WriteLine("He liked the name " + characterName);           
+            Console.WriteLine("But did not like the age " + characterAge);
+            Console.ReadLine();
+        }
+    }
+}
